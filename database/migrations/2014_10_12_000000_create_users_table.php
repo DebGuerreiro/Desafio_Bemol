e3d36c5925d386745afe6aa924d6ae2a397a7f82
Debora Guerreiro <dpg@icomp.ufam.edu.br>
2021-05-11 21:20:47 -0400

    ad migrate para login e cadastro

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cpf', 20)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
